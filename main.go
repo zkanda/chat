f84c2a9fff98451f5744b1d076023b70076ecd29
zkanda <hi@zkanda.io>
2017-05-12 18:25:23 +0800

    First web app

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+				<head>
+					<title>Chat</title>
+				</head>
+				<body>
+					Let's chat!
+				</body
+			</html>
+		`))
+	})
+	// start the web server
+	if err := http.ListenAndServe(":8089", nil); err != nil {
+		log.Fatal("ListendAndServe:", err)
+	}
+}
