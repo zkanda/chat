ed9c69281f79b1bab7088a4636dc8450f8379584
zkanda <hi@zkanda.io>
2017-05-13 23:08:25 +0800

    Display the data from in frontend

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 	"path/filepath"
 	"sync"
 
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 	"github.com/zkanda/trace"
 )
 
@@ -24,16 +27,32 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+	// setup gomniauth
+	gomniauth.SetSecurityKey("skjadhskjahdf9oy9328y89hfildsah")
+	gomniauth.WithProviders(
+		google.New(
+			os.Getenv("CHAT_KEY"),
+			os.Getenv("CHAT_SECRET"),
+			"http://localhost:8080/auth/callback/google",
+		),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
